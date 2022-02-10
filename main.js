bebc4006e941d589a6a73319a8c7ce917844d92e
John Carlo Treppo <johncarlotreppo@Johns-MacBook-Pro.local>
2022-02-10 14:33:41 -0500

    did the work

--- main.js
+++ main.js
@@ -9,6 +9,20 @@ Modified: 12/06/21
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
-function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+function button1Clicked(element) {
+    let randomNumber = Math.floor(Math.random() * 1000);
+    console.log("Button 1 was clicked: " + randomNumber); 
+    document.getElementById(element).innerHTML = randomNumber;
+}
+
+let blue = true
+function button2Clicked(element) {
+    let circle = document.getElementById(element);
+    if (blue) {
+        circle.style.fill = "green";
+        blue = false
+    } else {
+        circle.style.fill = "blue";
+        blue = true
+    } 
 }
